f6da87a6bbb4b40126bc369032f135680a9cea5e
tanmaytt <41143091+tanmaytt@users.noreply.github.com>
2020-09-30 21:00:01 +0530

    Update psuh.cpp
    Added comments for better understanding, just helping buddy, yay!

--- psuh.cpp
+++ psuh.cpp
@@ -5,18 +5,21 @@ struct node{
 	int data;
 	struct node *next;
 };
+/*Function used to print all the elements of stack.*/
 void print(struct node* head){
 	while(head!=NULL){
 		cout<<head->data<<" ";
 		head=head->next;
 	}
 }
+/*Function used to create a new node*/
 struct node* newnode(int data){
 	struct node *node=(struct node*)malloc(sizeof(struct node));
 	node->data=data;
 	node->next=NULL;
 	return node;
 }
+/*Function used to push node/element in stack.*/
 struct node* push(struct node *head,int data){
 	struct node *stacknode =newnode(data);
 	stacknode->next=head;
